b1549e89619bc4ad7fe4132e8929799b23a5b17c
Anastasia149 <anastasiya40149@gmail.com>
2025-04-02 19:45:49 +0300

    first commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Courses.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Courses.Data
+{
+    public class AppDbContext : IdentityDbContext<User>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
